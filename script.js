a071acce2c6ed6f60e446b49b9c7683579bca6ae
Joseph-J-Wang <wangjingao@cca.edu>
2025-06-16 22:20:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let map = L.map('map').setView([37.7749, -122.4194], 13);
 
-// 只保留白天瓦片
-let tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-tileLayer.addTo(map);
+const tiles = {
+    "day": L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
+    "night": L.tileLayer('https://basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png')
+};
+
+let currentTile = "day";
+tiles[currentTile].addTo(map);
+
+function switchDay() {
+    if (currentTile !== "day") {
+        map.removeLayer(tiles[currentTile]);
+        currentTile = "day";
+        tiles[currentTile].addTo(map);
+    }
+}
+
+function switchNight() {
+    if (currentTile !== "night") {
+        map.removeLayer(tiles[currentTile]);
+        currentTile = "night";
+        tiles[currentTile].addTo(map);
+    }
+}
